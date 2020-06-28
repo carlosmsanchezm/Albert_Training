18019b1cee01eefad34dffe4f5529218396dc15a
carlosmsanchezm <67176642+carlosmsanchezm@users.noreply.github.com>
2020-06-28 18:48:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ def get_balance():
     body=json.loads(str(request.data, encoding="utf-8"))
     print(json.dumps(body))
     
-    accounts = [{'checking' : '500',
+    accounts = {'checking' : '500',
                 'savings' : '1000'
-               }];
+               };
     
     # Check to see if state equals account_transfer
     if body["state"] == "get_balance":
@@ -56,7 +56,7 @@ def get_balance():
                 # fetch the account balance for the corresponding source value. Add a new balance property to the 
                 # _SOURCE_ACCOUNT_ slot. Set the balance value to the balance found in step 3.
                 body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"] = accounts
-                body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["value"] = body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["tokens"]
+                body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"]["value"] = body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"]["tokens"]
                 #If it is not a valid account type, set an error property to invalid account type.                                 
             elif body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["tokens"] != "Checking":
                 body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["error"] == "invalid"
