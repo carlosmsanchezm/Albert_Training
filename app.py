52f8a86f1601188b65da3cfae3d4841099594c82
carlosmsanchezm <67176642+carlosmsanchezm@users.noreply.github.com>
2020-06-28 18:12:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ def get_balance():
     body=json.loads(str(request.data, encoding="utf-8"))
     print(json.dumps(body))
     
-    accounts = {'checking' : '500',
+    accounts = [{'checking' : '500',
                 'savings' : '1000'
-               };
+               }];
     
     # Check to see if state equals account_transfer
     if body["state"] == "get_balance":
