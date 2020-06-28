414fd5667d362b912f60aed2e30dea6159242659
carlosmsanchezm <67176642+carlosmsanchezm@users.noreply.github.com>
2020-06-28 18:52:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_balance():
                 # fetch the account balance for the corresponding source value. Add a new balance property to the 
                 # _SOURCE_ACCOUNT_ slot. Set the balance value to the balance found in step 3.
                 body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"] = accounts
-                body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"]["value"] = body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"]["tokens"]
+                body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["balance"]["value"] = body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["tokens"]
                 #If it is not a valid account type, set an error property to invalid account type.                                 
             elif body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["tokens"] != "Checking":
                 body["slots"]["_SOURCE_ACCOUNT_"]["values"][0]["error"] == "invalid"
